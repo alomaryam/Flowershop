be95c4d4ded4646e91cbf72e3755fc9193960dcb
alomaryam <alomaryam@gmail.com>
2021-02-16 03:01:53 +0300

    task 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import flowershop from "./flowershop.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="top">
+      <h1>Florescence</h1>
+      <p>Checkout our Fresh Blooms!</p>
+      <img alt="flowershop" src={flowershop} />
     </div>
   );
 }
