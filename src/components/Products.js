3a83ba4dad51accb3f3e451628cc118d2addc059
alomaryam <alomaryam@gmail.com>
2021-03-11 17:53:37 +0300

    fetchitemsdone

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,37 +1,37 @@
-import img1 from "./images/img1.JPG";
-import img2 from "./images/img2.JPG";
-import img3 from "./images/img3.JPG";
-import img4 from "./images/img4.JPG";
+// import img1 from "./images/img1.JPG";
+// import img2 from "./images/img2.JPG";
+// import img3 from "./images/img3.JPG";
+// import img4 from "./images/img4.JPG";
 
-const Flowers = [
-  {
-    id: 1,
-    name: "Baby Pink Vase",
-    price: 30,
-    image: img1,
-    description: "customized vases upon request",
-  },
-  {
-    id: 2,
-    name: "Light Wooden Vase",
-    price: 35,
-    image: img2,
-    description: "customized vases upon request",
-  },
-  {
-    id: 3,
-    name: "Glass Vase (small)",
-    price: 25,
-    image: img3,
-    description: "customized vases upon request",
-  },
-  {
-    id: 4,
-    name: "Glass Vase (large)",
-    price: 40,
-    image: img4,
-    description: "customized vases upon request",
-  },
-];
+// const Flowers = [
+//   {
+//     id: 1,
+//     name: "Baby Pink Vase",
+//     price: 30,
+//     image: img1,
+//     description: "customized vases upon request",
+//   },
+//   {
+//     id: 2,
+//     name: "Light Wooden Vase",
+//     price: 35,
+//     image: img2,
+//     description: "customized vases upon request",
+//   },
+//   {
+//     id: 3,
+//     name: "Glass Vase (small)",
+//     price: 25,
+//     image: img3,
+//     description: "customized vases upon request",
+//   },
+//   {
+//     id: 4,
+//     name: "Glass Vase (large)",
+//     price: 40,
+//     image: img4,
+//     description: "customized vases upon request",
+//   },
+// ];
 
-export default Flowers;
+// export default Flowers;
--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,19 +1,32 @@
-import flowers from "../components/Products.js";
+// import flowers from "../components/Products.js";
 import { makeObservable, observable, action } from "mobx";
+import axios from "axios";
 
 class ProductStore {
-  flowers = flowers;
+  // flowers = flowers;
+  flowers = [];
 
   deleteFlower = (id) => {
     this.flowers = this.flowers.filter((flowerid) => flowerid.id !== id);
+  };
+
+  fetchFlowers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8001/flowers");
+      this.flowers = response.data;
+    } catch (error) {
+      console.error("productStore -> fetchFlowers -> error", error);
+    }
   };
 
   constructor() {
     makeObservable(this, {
       flowers: observable,
       deleteFlower: action,
+      fetchFlowers: action,
     });
   }
 }
 const productStore = new ProductStore();
+productStore.fetchFlowers();
 export default productStore;
